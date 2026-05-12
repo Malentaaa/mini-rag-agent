eecd70c6091504fe59f2216c8453dd06eba2ed63
Kseniya Tolstokorova <malentaaa@gmail.com>
2026-05-12 19:30:50 +0300

    добавлен индекс сервис в main

--- app/main.py
+++ app/main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
 
 from app.api.rag_router import router
+from app.services.index_service import IndexService
 
 app = FastAPI(
     title="Mini RAG Agent"
 )
+
+index_service = IndexService()
+index_service.ensure_index_exists()
+
 app.include_router(router)
