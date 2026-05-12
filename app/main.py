0cee0c19579401fb6518c17e03deb5d3ccb8e3ae
Kseniya Tolstokorova <malentaaa@gmail.com>
2026-05-12 21:30:59 +0300

    добавлен веб интерфейс и улучшена структура проекта

--- app/main.py
+++ app/main.py
@@ -1,13 +1,66 @@
 from fastapi import FastAPI
+from fastapi import Request
+from fastapi import Form
+
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 from app.api.rag_router import router
 from app.services.index_service import IndexService
+from app.services.rag_service import RAGService
+from app.core.config import TOP_K
+
 
 app = FastAPI(
     title="Mini RAG Agent"
 )
 
+app.include_router(router)
+
+app.mount(
+    "/static",
+    StaticFiles(directory="app/static"),
+    name="static"
+)
+
+templates = Jinja2Templates(
+    directory="app/templates"
+)
+
 index_service = IndexService()
 index_service.ensure_index_exists()
+rag_service = RAGService()
 
-app.include_router(router)
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+
+    return templates.TemplateResponse(
+        request,
+        "index.html",
+        {
+            "query": "",
+            "top_k": TOP_K,
+            "results": None
+        }
+    )
+    
+@app.post("/", response_class=HTMLResponse)
+def ask(
+    request: Request,
+    query: str = Form(...),
+    top_k: int = Form(5)
+):
+    results = rag_service.ask(
+        query=query,
+        top_k=top_k
+    )
+    return templates.TemplateResponse(
+        request,
+        "index.html",
+        {
+            "query": query,
+            "top_k": top_k,
+            "results": results
+        }
+    )
---
+++ app/services/rag_service.py
@@ -0,0 +1,25 @@
+from app.pipeline.query_pipeline import QueryPipeline
+
+from app.core.config import (
+    INDEX_PATH,
+    CHUNKS_PATH,
+    TOP_K
+)
+
+class RAGService:
+    def __init__(self):
+
+        self.pipeline = QueryPipeline()
+
+    def ask(
+        self,
+        query: str,
+        top_k: int = TOP_K
+    ):
+        results = self.pipeline.run(
+            query=query,
+            index_path=str(INDEX_PATH),
+            chunks_path=str(CHUNKS_PATH),
+            top_k=top_k
+        )
+        return results
