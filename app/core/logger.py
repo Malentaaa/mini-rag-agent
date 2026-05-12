f124660c2832c1077970ae0ce18c5853aeec4e8e
Kseniya Tolstokorova <malentaaa@gmail.com>
2026-05-12 19:52:13 +0300

    добавлен лог модуль

---
+++ app/core/logger.py
@@ -0,0 +1,32 @@
+import logging
+
+from app.core.config import (
+    LOGS_DIR,
+    LOG_FILE
+)
+
+LOGS_DIR.mkdir(exist_ok=True)
+
+
+def get_logger(name: str) -> logging.Logger:
+    logger = logging.getLogger(name)
+
+    if logger.handlers:
+        return logger
+
+    logger.setLevel(logging.INFO)
+
+    formatter = logging.Formatter(
+        "%(asctime)s | %(levelname)s | %(name)s | %(message)s"
+    )
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+
+    file_handler = logging.FileHandler(LOG_FILE, encoding="utf-8")
+    file_handler.setFormatter(formatter)
+
+    logger.addHandler(console_handler)
+    logger.addHandler(file_handler)
+
+    return logger
--- app/services/index_service.py
+++ app/services/index_service.py
@@ -6,22 +6,25 @@ from app.core.config import (
     INDEX_PATH,
     CHUNKS_PATH
 )
+from app.core.logger import get_logger
+
+logger = get_logger(__name__)
 
 class IndexService:
     def __init__(self):
         self.pipeline = IndexingPipeline()
 
     def ensure_index_exists(self):
-        index_exists = Path(INDEX_PATH).exists()
-        chunks_exist = Path(CHUNKS_PATH).exists()
+        index_exists = INDEX_PATH.exists()
+        chunks_exist = CHUNKS_PATH.exists()
         if index_exists and chunks_exist:
-            print("\nINDEX FOUND")
+            logger.info("Index found")
             return
-        print("\nINDEX NOT FOUND")
-        print("CREATING INDEX...")
+        logger.info("Index not found")
+        logger.info("Creating index")
         self.pipeline.run(
             docs_path=str(DOCS_PATH),
             index_path=str(INDEX_PATH),
             chunks_path=str(CHUNKS_PATH)
         )
-        print("\nINDEX CREATED")
+        logger.info("Index created")
